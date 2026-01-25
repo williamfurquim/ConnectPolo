b7c7968ff2f24fb747c5d7c8368be8effe32a806
William Furquim <w.furquim@aluno.senai.br>
2026-01-25 02:43:57 -0300

    Puxar imagem do líder

--- JS/Perfil/dados-lider.js
+++ JS/Perfil/dados-lider.js
@@ -10,6 +10,7 @@ const contrato = document.getElementById("n-contrato");
 const nascimento = document.getElementById("dataNascimento");
 const email = document.getElementById("email");
 const telefone = document.getElementById("telefone");
+const imagem = document.getElementById("imagem");
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) return;
@@ -26,4 +27,5 @@ onAuthStateChanged(auth, async (user) => {
   nascimento.value = d.dataNascimento || "------";
   email.value = d.email || "-----";
   telefone.value = d.telefone || "-----";
+  imagem.src = d.foto || "https://cdn-icons-png.flaticon.com/512/149/149071.png"; 
 });
